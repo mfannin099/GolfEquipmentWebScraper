71e86b311abbf719751b38d370c0ea16024a2339
mfannin <mattfannin6@gmail.com>
2026-03-06 16:42:58 -0500

    main updated

--- notebooks/test_script_detroit_putter.py
+++ notebooks/test_script_detroit_putter.py
@@ -11,32 +11,35 @@ sys.path.append('..')
 from detroit_putter_co_class import DetroitPutterScraper, DetroitAccessoryScraper
 
 filepath_to_check = r"../data/detroit_putters.csv"
-filepath_to_check2 = r"../data/detroit_putters.csv"
+filepath_to_check2 = r"../data/detroit_accessories.csv"
 
-if os.path.exists(filepath_to_check):
+if os.path.exists(filepath_to_check) and os.path.exists(filepath_to_check2):
     print("Data already exists, loading from file...")
     df = pd.read_csv(filepath_to_check)
     print(df)
     print(df.columns)
     print(df.shape)
+
+    df2 = pd.read_csv(filepath_to_check2)
+    print(df2)
+    print(df2.columns)
+    print(df2.shape)
 else:
-    putter_scraper = DetroitPutterScraper()
-    df=putter_scraper.run()
-    putter_scraper.save(filename="detroit_putters", filepath=r"../data/")
+    if not os.path.exists(filepath_to_check):
+        print("Putter data not found, scraping...")
+        putter_scraper = DetroitPutterScraper()
+        df = putter_scraper.run()
+        putter_scraper.save(filename="detroit_putters", filepath=r"../data/")
+    else:
+        print("Putter data already exists, loading from file...")
+        df = pd.read_csv(filepath_to_check)
 
-acc_scaper = DetroitAccessoryScraper()
-df=acc_scaper.run()
-print(df)
+    if not os.path.exists(filepath_to_check2):
+        print("Accessory data not found, scraping...")
+        acc_scraper = DetroitAccessoryScraper()
+        df2 = acc_scraper.run()
+        acc_scraper.save(filename="detroit_accessories", filepath=r"../data/")
+    else:
+        print("Accessory data already exists, loading from file...")
+        df2 = pd.read_csv(filepath_to_check2)
 
-# filepath_to_check = r"../data/detroit_putters.csv"
-
-# if os.path.exists(filepath_to_check):
-#     print("Data already exists, loading from file...")
-#     df = pd.read_csv(filepath_to_check)
-#     print(df)
-#     print(df.columns)
-#     print(df.shape)
-# else:
-#     putter_scraper = DetroitPutterScraper()
-#     df=putter_scraper.run()
-#     putter_scraper.save(filename="detroit_putters", filepath=r"../data/")
